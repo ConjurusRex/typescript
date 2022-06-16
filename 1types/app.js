968cc7aba80e4370e78fe92c6e8d439f2b848051
ConjurusRex <jnebeker91@protonmail.com>
2022-06-16 16:58:57 -0600

    adds 1types to dir, adds index file with related ts and js files

---
+++ 1types/app.js
@@ -0,0 +1,7 @@
+var fn = function (num, num2) {
+    return num + num2;
+};
+var num = 1;
+var num2 = 1;
+var res = fn(num, num2);
+console.log(res);
---
+++ 1types/app.ts
@@ -0,0 +1,8 @@
+let fn = (num: number, num2: number) => {
+  return num + num2;
+}
+
+let num = 1;
+let num2 = 1;
+let res = fn(num, num2);
+console.log(res);
