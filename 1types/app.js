bb2b8a717cf65195b1c56113bc99bdbeef95d891
ConjurusRex <jnebeker91@protonmail.com>
2022-06-21 16:48:05 -0600

    adds examples for obj, array, tuple, enum, union, and literal types

--- 1types/app.js
+++ 1types/app.js
@@ -1,7 +1,78 @@
 var fn = function (num, num2) {
     return num + num2;
 };
+// let num: number; -> do this for unassigned variables
 var num = 1;
 var num2 = 1;
 var res = fn(num, num2);
-console.log(res);
+// console.log(res);
+// let worker: {
+//   name: string;
+//   age: number;
+//   likes: string[]
+// } = {
+var worker = {
+    name: 'josh',
+    age: 31,
+    likes: ['music', 'code']
+};
+// let stuff: any; -> not best practice to use type 'any'
+var stuff;
+stuff = ['slime', 2, 'string', 4, 1, 4, 5];
+var queen = {
+    name: 'jess',
+    age: 32,
+    likes: ['drawing', 'reading'],
+    dish: [1, 'loaded peppers']
+};
+// enum Spells { FROST = 1, FIRE, ARCANE, SHADOW = 'shadow' }
+var Spells;
+(function (Spells) {
+    Spells[Spells["FROST"] = 0] = "FROST";
+    Spells[Spells["FIRE"] = 1] = "FIRE";
+    Spells[Spells["ARCANE"] = 2] = "ARCANE";
+    Spells[Spells["SHADOW"] = 3] = "SHADOW";
+})(Spells || (Spells = {}));
+;
+var conjurer = {
+    primary: Spells.FROST,
+    secondary: Spells.FIRE
+};
+if (conjurer.primary == 0) {
+    // console.log('conjurer is frost');
+}
+var fn2 = function (num, num2) {
+    if (typeof num === 'number' && typeof num2 === 'number') {
+        return num + num2;
+    }
+    else {
+        return num.toString() + ' ' + num2.toString();
+    }
+};
+var res1 = fn2(1, 1);
+var res2 = fn2('jess', 'josh');
+var res3 = fn2(1, 'josh');
+// console.log(res1, res2, res3);
+var fn3 = function (num, num2, result) {
+    var res;
+    if (typeof num === 'number' && typeof num2 === 'number' || result === 'as-number') {
+        res = +num + +num2;
+    }
+    else {
+        res = num.toString() + num2.toString();
+    }
+    return res;
+    // if (result === 'as-number') {
+    //   return +res;
+    // } else if (result === 'as-string') {
+    //   return res.toString();
+    // }
+};
+var res4 = fn3(1, 1, 'as-number');
+var res5 = fn3('jess', 'josh', 'as-string');
+var res6 = fn3('1', '1', 'as-number');
+var res7 = fn3('1', 2, 'as-number');
+// console.log(res4);
+// console.log(res5);
+// console.log(res6);
+// console.log(res7);
--- 1types/app.ts
+++ 1types/app.ts
@@ -2,7 +2,87 @@ let fn = (num: number, num2: number) => {
   return num + num2;
 }
 
+// let num: number; -> do this for unassigned variables
 let num = 1;
 let num2 = 1;
-let res = fn(num, num2);
-console.log(res);
+const res = fn(num, num2);
+// console.log(res);
+
+
+// let worker: {
+//   name: string;
+//   age: number;
+//   likes: string[]
+// } = {
+let worker = {
+  name: 'josh',
+  age: 31,
+  likes: ['music', 'code'],
+}
+
+
+// let stuff: any; -> not best practice to use type 'any'
+let stuff: (string | number)[];
+stuff = ['slime', 2, 'string', 4, 1, 4, 5];
+
+
+let queen: {
+  name: string;
+  age: number;
+  likes: string[];
+  dish: [number, string]; // 'tuple' type
+} = {
+  name: 'jess',
+  age: 32,
+  likes: ['drawing', 'reading'],
+  dish: [1, 'loaded peppers'],
+}
+
+
+// enum Spells { FROST = 1, FIRE, ARCANE, SHADOW = 'shadow' }
+enum Spells { FROST, FIRE, ARCANE, SHADOW };
+let conjurer = {
+  primary: Spells.FROST,
+  secondary: Spells.FIRE,
+}
+if (conjurer.primary == 0) {
+  // console.log('conjurer is frost');
+}
+
+
+let fn2 = (num: number | string, num2: number | string) => {
+  if (typeof num === 'number' && typeof num2 === 'number') {
+    return num + num2;
+  } else {
+    return num.toString() + ' ' + num2.toString();
+  }
+}
+const res1 = fn2(1, 1);
+const res2 = fn2('jess', 'josh');
+const res3 = fn2(1, 'josh');
+// console.log(res1, res2, res3);
+
+
+let fn3 = (num: number | string, num2: number | string, result: 'as-number' | 'as-string') => { // result is  a 'literal' type
+  let res;
+  if (typeof num === 'number' && typeof num2 === 'number' || result === 'as-number') {
+    res = +num + +num2;
+  } else {
+    res = num.toString() + num2.toString();
+  }
+  return res;
+  // if (result === 'as-number') {
+  //   return +res;
+  // } else if (result === 'as-string') {
+  //   return res.toString();
+  // }
+}
+const res4 = fn3(1, 1, 'as-number');
+const res5 = fn3('jess', 'josh', 'as-string');
+const res6 = fn3('1', '1', 'as-number');
+const res7 = fn3('1', 2, 'as-number');
+
+// console.log(res4);
+// console.log(res5);
+// console.log(res6);
+// console.log(res7);
