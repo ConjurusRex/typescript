7268f68e5446bc17ddce2fd2097fbda88cf93eb2
ConjurusRex <jnebeker91@protonmail.com>
2022-06-22 16:26:21 -0600

    adds aliases, object, function, callback, unknown, void, never, and any types

--- 1types/app.js
+++ 1types/app.js
@@ -72,7 +72,76 @@ var res4 = fn3(1, 1, 'as-number');
 var res5 = fn3('jess', 'josh', 'as-string');
 var res6 = fn3('1', '1', 'as-number');
 var res7 = fn3('1', 2, 'as-number');
-// console.log(res4);
-// console.log(res5);
-// console.log(res6);
-// console.log(res7);
+var fn4 = function (num, num2, result) {
+    var res;
+    if (typeof num === 'number' && typeof num2 === 'number' || result === 'as-number') {
+        res = +num + +num2;
+    }
+    else {
+        res = num.toString() + num2.toString();
+    }
+    return res;
+};
+var fn5 = function (robot) {
+    console.log('hi my name is ' + robot.name + ', and i am ' + robot.age + ' years old.');
+};
+var robo = {
+    name: 'robo',
+    age: 4120
+};
+// fn5(robo);
+// fn return type
+var fn6 = function (num, num2) {
+    return num + num2;
+};
+// fn void return type -> does not require return statement
+var fn7 = function (num) {
+    console.log(num);
+};
+// fn7(fn6(2000, 22));
+// console.log(fn7(fn6(2000, 22))); -> this returns 'undefined'
+// let value: undefined; -> undefined is a valid type in TS, void is the absence of any type at all
+// this is a valid function as undefined requires a return statement
+// let fn7 = (num: number): undefined => {
+//   console.log(num);
+//   return;
+// }
+// function types
+var add;
+add = fn6;
+// console.log(add(6, 4));
+// function type definition -> better at catching errors during compilation
+var subtract;
+var fn8 = function (num, num2) {
+    return num - num2;
+};
+subtract = fn8;
+// console.log(subtract(10, 5));
+// function callback types
+var addAndPrint = function (num, num2, cb) {
+    var res = num + num2;
+    cb(res);
+};
+// addAndPrint(10, 10, (res) => {
+//   console.log(res);
+// })
+// unknown type
+var userInput;
+// setting userInput to type any will not throw any errors/warnings
+// let userInput: any;
+var username;
+userInput = 'josh';
+// unknown types cannot be assigned to string type variables
+// username = userInput;
+// must type check to assign unknown variable to string type variable
+if (typeof userInput == 'string') {
+    username = userInput;
+}
+// never type -> never produces/returns a value
+var fn9 = function (message, code) {
+    throw {
+        message: message,
+        code: code
+    };
+};
+fn9('an error occured', 510);
--- 1types/app.ts
+++ 1types/app.ts
@@ -86,3 +86,98 @@ const res7 = fn3('1', 2, 'as-number');
 // console.log(res5);
 // console.log(res6);
 // console.log(res7);
+
+
+// type aliases
+type Combined = number | string;
+type Conversion = 'as-number' | 'as-string';
+let fn4 = (num: Combined, num2: Combined, result: Conversion) => { // result is  a 'literal' type
+  let res;
+  if (typeof num === 'number' && typeof num2 === 'number' || result === 'as-number') {
+    res = +num + +num2;
+  } else {
+    res = num.toString() + num2.toString();
+  }
+  return res;
+}
+
+
+// object types
+type Robot = {
+  name: string;
+  age: number;
+}
+let fn5 = (robot: Robot) => {
+  console.log('hi my name is ' + robot.name + ', and i am ' + robot.age + ' years old.');
+}
+let robo: Robot = {
+  name: 'robo',
+  age: 4120
+}
+// fn5(robo);
+
+
+// fn return type
+let fn6 = (num: number, num2: number): number => {
+  return num + num2;
+}
+// fn void return type -> does not require return statement
+let fn7 = (num: number) => {
+  console.log(num);
+}
+// fn7(fn6(2000, 22));
+// console.log(fn7(fn6(2000, 22))); -> this returns 'undefined'
+// let value: undefined; -> undefined is a valid type in TS, void is the absence of any type at all
+// this is a valid function as undefined requires a return statement
+// let fn7 = (num: number): undefined => {
+//   console.log(num);
+//   return;
+// }
+
+
+// function types
+let add: Function;
+add = fn6;
+// console.log(add(6, 4));
+
+// function type definition -> better at catching errors during compilation
+let subtract: (a: number, b: number) => number;
+let fn8 = (num: number, num2: number) => {
+  return num - num2;
+}
+subtract = fn8;
+// console.log(subtract(10, 5));
+
+
+// function callback types
+let addAndPrint = (num: number, num2: number, cb: (num: number) => void) => {
+  const res = num + num2;
+  cb(res);
+}
+// addAndPrint(10, 10, (res) => {
+//   console.log(res);
+// })
+
+
+// unknown type
+let userInput: unknown;
+// setting userInput to type any will not throw any errors/warnings
+// let userInput: any;
+let username: string;
+userInput = 'josh';
+// unknown types cannot be assigned to string type variables
+// username = userInput;
+// must type check to assign unknown variable to string type variable
+if (typeof userInput == 'string') {
+  username = userInput;
+}
+
+
+// never type -> never produces/returns a value
+let fn9 = (message: string, code: number) => {
+  throw {
+    message,
+    code,
+  };
+}
+fn9('an error occured', 510);
