7e00e5c8c6c0a0c79865f5bb2400e257645bc2e5
ConjurusRex <jnebeker91@protonmail.com>
2022-06-23 17:14:34 -0600

    tsconfig update, adds example using ! and ? operator

--- 2config/app.ts
+++ 2config/app.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+const button = document.querySelector('button'); // add '!' to let typescript know that button will not be null
+
+const fn = (message: string) => {
+  return console.log(message);
+}
+
+// typescript adds '?' after button to indicate button could be HTMLButtonElement | null
+// button?.addEventListener('click', fn.bind('data was successfully sent'));
+// type check to get around ! and ?
+if (button) {
+  button.addEventListener('click', fn.bind(null, 'data was successfully sent'));
+}
--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hello world');
+const button = document.querySelector('button'); // add '!' to let typescript know that button will not be null
+const fn = (message) => {
+    return console.log(message);
+};
+// typescript adds '?' after button to indicate button could be HTMLButtonElement | null
+// button?.addEventListener('click', fn.bind('data was successfully sent'));
+// type check to get around ! and ?
+if (button) {
+    button.addEventListener('click', fn.bind(null, 'data was successfully sent'));
+}
