dc01bd452ad7b5bcb544c51b02d78547a5136ac6
ConjurusRex <jnebeker91@protonmail.com>
2022-06-28 16:56:41 -0600

    adds 3es6 dir with app.ts file, updates tsconfig

--- 3es6/app.ts
+++ 3es6/app.ts
@@ -1,23 +1,38 @@
-"use strict";
 // spread
 let hobbies = ['reading', 'writing', 'music'];
 let extraHobbies = [...hobbies, 'painting'];
 console.log(extraHobbies);
+
 let person = {
-    name: 'josh',
-    age: 31,
+  personName: 'josh',
+  age: 31,
 };
-let copiedPerson = Object.assign({ id: 1 }, person);
+
+let copiedPerson = { 
+  id: 1,
+  ...person,
+};
 console.log(copiedPerson);
+
+
 // rest
 // (...numbers: number[number, number, number]) -> this would be an example using the tuple type
-const add = (...numbers) => {
-    return numbers.reduce((res, value) => {
-        return res + value;
-    }, 0);
+const add = (...numbers: number[]) => {
+  return numbers.reduce((res, value) => {
+    return res + value;
+  }, 0);
 };
+
 const addedNumbers = add(5, 5, 5, 5, 5);
 console.log(addedNumbers);
+
+
 // destructuring
+// destructures in index order
 const [hobby1, hobby2, ...remainingHobbies] = hobbies;
 console.log(hobby1, hobby2, remainingHobbies);
+
+// destructures by key name
+// { personName: username } -> can override key names -> console.log(username, age);
+const { personName, age } = person;
+console.log(personName, age);
