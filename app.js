3c8b322e9fab9e962af083caae6aba1b4b9ea243
ConjurusRex <jnebeker91@protonmail.com>
2022-07-04 18:24:32 -0600

    adds 6generics dir

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
 "use strict";
-const move = (animal) => {
-    let speed;
-    // typescript will infer the available type properties
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('moving speed: ' + speed);
-};
-// typescript will infer the available type properties
-move({ type: 'bird', flyingSpeed: 20 });
-move({ type: 'horse', runningSpeed: 30 });
